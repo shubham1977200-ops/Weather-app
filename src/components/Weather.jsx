c919e26a05ea7e1d8be80cb0f39efd26ac0fb36b
shubham1977200-ops <shubham1977200@gmail.com>
2026-08-18 17:08:30 +0530

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -69,7 +69,11 @@ const Weather = () => {
   return (
     <div className="weather">
       <div className="search-bar">
-        <input ref ={inputRef}type="text" placeholder='Search' />
+        <input ref ={inputRef}type="text" placeholder='Search' onKeyDown={(e)=>{
+          if(e.key==='Enter'){
+            search(inputRef.current.value);
+          }
+        }} />
         <img src={search_icon} alt="" onClick={()=> search(inputRef.current.value)} />
       </div>
       {weatherData?<>
